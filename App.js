137d6620694e5f566ed1dc0d8fa011ec9173bdef
waleed ibrahim <iwaleedibrahim@gmail.com>
2020-01-22 10:07:52 +0200

    adding todo list component and vector icons

--- App.js
+++ App.js
@@ -3,49 +3,67 @@ import {
   StyleSheet,
   Text,
   View,
+  TouchableOpacity,
   TextInput,
-  Image,
-  TouchableHighlight,
-  Alert,
   ScrollView,
 } from 'react-native';
+
+import Icon from 'react-native-vector-icons/Feather';
 import TodoList from './TodoList';
 
-const addImage = require('./assets/Images/plus.png');
-
 export default function App() {
-  const [singleTodo, setSingleTodo] = useState('');
-  const [todos, insertIntoTodos] = useState([]);
-  addNewTodo = () => {
-    if (singleTodo.length > 0) {
-      insertIntoTodos([
-        ...todos,
-        { text: singleTodo, key: Date.now().toString(), checked: false },
-      ]);
-      setSingleTodo('');
-      Alert.alert('done!', 'todo was added');
+  const [value, setValue] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  addTodo = () => {
+    if (value.length > 0) {
+      setTodos([...todos, { text: value, key: Date.now(), checked: false }]);
+      setValue('');
     }
   };
+
+  checkTodo = id => {
+    setTodos(
+      todos.map(todo => {
+        if (todo.key === id) todo.checked = !todo.checked;
+        return todo;
+      }),
+    );
+  };
+
+  deleteTodo = id => {
+    setTodos(
+      todos.filter(todo => {
+        if (todo.key !== id) return true;
+      }),
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>{'Todo List'}</Text>
+      <Text style={styles.header}>Todo List</Text>
       <View style={styles.textInputContainer}>
         <TextInput
           style={styles.textInput}
           multiline={true}
           placeholder="What do you want to do today?"
           placeholderTextColor="#abbabb"
-          value={singleTodo}
+          value={value}
+          onChangeText={value => setValue(value)}
         />
-        <TouchableHighlight
-          onPress={() => Alert.alert('done!', 'todo was deleted')}
-          underlayColor={'transpatent'}>
-          <Image source={addImage} style={styles.trash} />
-        </TouchableHighlight>
+        <TouchableOpacity onPress={() => addTodo()}>
+          <Icon name="plus" size={30} color="blue" style={{ marginLeft: 15 }} />
+        </TouchableOpacity>
       </View>
       <ScrollView style={{ width: '100%' }}>
         {todos.map(item => (
-          <TodoList text={item.text} key={item.key} />
+          <TodoList
+            text={item.text}
+            key={item.key}
+            checked={item.checked}
+            setChecked={() => checkTodo(item.key)}
+            deleteTodo={() => deleteTodo(item.key)}
+          />
         ))}
       </ScrollView>
     </View>
@@ -62,29 +80,24 @@ const styles = StyleSheet.create({
   header: {
     marginTop: '15%',
     fontSize: 20,
-    color: '#51008f',
+    color: 'red',
     paddingBottom: 10,
   },
   textInputContainer: {
     flexDirection: 'row',
-    alignItems: 'center',
-    // justifyContent: 'center',
+    alignItems: 'baseline',
     borderColor: 'black',
     borderBottomWidth: 1,
-    paddingVertical: 0,
-    paddingHorizontal: 5,
+    paddingRight: 10,
+    paddingBottom: 10,
   },
   textInput: {
     flex: 1,
-    height: 50,
+    height: 20,
     fontSize: 18,
     fontWeight: 'bold',
     color: 'black',
     paddingLeft: 10,
     minHeight: '3%',
-  },
-  trash: {
-    height: 35,
-    width: 35,
   },
 });
--- TodoList.js
+++ TodoList.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import {
-  StyleSheet,
-  TouchableOpacity,
-  View,
-  Text,
-  TouchableHighlight,
-} from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 export default function TodoList(props) {
   return (
     <View style={styles.listContainer}>
-      {/* <Icon name="square" size={30} color="black" style={{ marginLeft: 15 }} /> */}
-      <Text style={styles.listItem}>{props.text}</Text>
-      <TouchableHighlight onPress={props.deleteTodo} underlayColor={'red'}>
-        <Image
-          source={deleteImage}
-          size={30}
-          color="black"
-          style={{ marginLeft: 15 }}
-          style={{ marginLeft: 'auto' }}
-        />
-      </TouchableHighlight>
+      <Icon
+        name={props.checked ? 'check' : 'square'}
+        size={30}
+        color="black"
+        style={{ marginLeft: 15 }}
+        onPress={props.setChecked}
+      />
+      <View>
+        {props.checked && <View style={styles.verticalLine} />}
+        <Text style={styles.listItem}>{props.text}</Text>
+      </View>
+      <Icon
+        name="trash-2"
+        size={30}
+        color="red"
+        style={{ marginLeft: 'auto' }}
+        onPress={props.deleteTodo}
+      />
     </View>
   );
 }
@@ -43,6 +45,15 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     fontSize: 17,
     fontWeight: 'bold',
-    color: 'white',
+    color: 'black',
+  },
+  verticalLine: {
+    borderBottomColor: 'green',
+    borderBottomWidth: 4,
+    marginLeft: 10,
+    width: '100%',
+    position: 'absolute',
+    marginTop: 15,
+    fontWeight: 'bold',
   },
 });
